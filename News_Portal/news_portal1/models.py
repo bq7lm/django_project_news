d9ba3d68367f080a82a8cc2f0c55eebb4f34f741
bq7lm <javan121007@gmail.com>
2024-10-20 20:46:37 +0300

    second commit

--- News_Portal/news_portal1/models.py
+++ News_Portal/news_portal1/models.py
@@ -20,6 +20,9 @@ class Author(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
     
+    def __str__(self):
+        return self.name.title()
+    
 class Post(models.Model):
     ARTICLE = 'AR'  
     NEWS = 'NW'  
@@ -44,6 +47,8 @@ class Post(models.Model):
     def preview(self):
         return self.content[:124] + ('...' if len(self.content) > 124 else '')  
     
+    def __str__(self):
+        return f'{self.title}: {self.content}'
    
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)  
---
+++ News_Portal/news_portal1/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+
+register = template.Library()
+
+WORDS = ['редиска','плохой','здоровье', 'информации']
+
+@register.filter()
+def censor(value):
+    if not isinstance(value, str):  
+        raise ValueError("Значение не строка")  
+    for word in WORDS:
+        censored_word = word[0] + '*' * (len(word) - 1)  
+        
+        value = value.replace(word, censored_word)  
+        value = value.replace(word.upper(), censored_word.upper())  
+        value = value.replace(word.capitalize(), censored_word.capitalize())  
+
+    return value 
+        
