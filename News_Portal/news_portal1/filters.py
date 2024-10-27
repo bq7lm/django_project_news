1e37452052c84a6d2e5341e9582dce3823a29d1b
bq7lm <javan121007@gmail.com>
2024-10-27 15:07:05 +0300

    add style

--- News_Portal/news_portal1/filters.py
+++ News_Portal/news_portal1/filters.py
@@ -1,10 +1,10 @@
-from django.forms import DateInput
+from django.forms import DateInput, TextInput
 from .models import Post
 from django_filters import FilterSet, CharFilter, DateFilter
 
 class NewsFilter(FilterSet):
     author = CharFilter(field_name='author__user__username', lookup_expr='icontains', label='Автор')
-    title = CharFilter(field_name='title', lookup_expr='icontains', label='Название')
+    title = CharFilter(field_name='title', lookup_expr='icontains', label='Название', widget=TextInput(attrs={'style': 'width:40%;'}))
     search_date = DateFilter(field_name='time_add', lookup_expr='gte', label='Публикации от',
                              widget=DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
 
