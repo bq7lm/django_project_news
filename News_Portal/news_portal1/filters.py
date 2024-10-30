748b2e5423fb7e1eff34857539c74b832503fec4
bq7lm <javan121007@gmail.com>
2024-10-30 22:25:01 +0300

    style main, not login

--- News_Portal/news_portal1/filters.py
+++ News_Portal/news_portal1/filters.py
@@ -4,7 +4,7 @@ from django_filters import FilterSet, CharFilter, DateFilter
 
 class NewsFilter(FilterSet):
     author = CharFilter(field_name='author__user__username', lookup_expr='icontains', label='Автор')
-    title = CharFilter(field_name='title', lookup_expr='icontains', label='Название', widget=TextInput(attrs={'style': 'width:40%;'}))
+    title = CharFilter(field_name='title', lookup_expr='icontains', label='Название', widget=TextInput(attrs={'style': 'width:100%;'}))
     search_date = DateFilter(field_name='time_add', lookup_expr='gte', label='Публикации от',
                              widget=DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
 
--- News_Portal/news_portal1/forms.py
+++ News_Portal/news_portal1/forms.py
@@ -1,22 +1,29 @@
-from django import forms
-from django.core.exceptions import ValidationError
-
-
-
+from django import forms  
+from django.core.exceptions import ValidationError  
 from .models import Post
 
 
 class NewsForm(forms.ModelForm):
-    content = forms.CharField(min_length=20)
+    content = forms.CharField(
+        min_length=20,
+        widget=forms.Textarea(attrs={
+            'style': 'width: 100%; min-height: 150px;'  # Установите минимальную высоту
+        })
+    )
+
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'style': 'width: 100%; line-height: 1.5;'
+        })
+    )
 
     class Meta:
         model = Post
-        fields = ['title', 'content','categories',]
-    
+        fields = ['title', 'content', 'categories',]
 
     def clean(self):
         cleaned_data = super().clean()
-        content = cleaned_data.get("content")
+        content = cleaned_data.get("content")  # Исправлено с "description" на "content"
         title = cleaned_data.get("title")
 
         if title == content:
